1075d88a577fad229a7f01443427916a6127fe93
Ilyas Gibadullin <i.gibadullin@gmail.com>
2022-12-15 18:26:16 +0200

    Docstrings added

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Метод атаки. Выдает урон. Присутсвует элемент случайности."""
     if char_class == 'warrior':
         return (
             f'{char_name} нанёс урон противнику равный'
@@ -26,6 +27,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Метод защиты. Выдает блокировку. Присутсвует элемент случайности."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -36,6 +38,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Специальные навыки."""
     if char_class == 'warrior':
         return (
             f'{char_name} применил специальное умение «Выносливость'
@@ -48,6 +51,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Начало тренировки."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -73,6 +77,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Выбор класса персонажа."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
